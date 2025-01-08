ed03a6d0882ed883f4510b3dfffc4c753e08f4e3
EjikeOdoh <ejike.developer@gmail.com>
2025-01-08 03:09:21 +0100

    Add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config'
+import {ConfigModule, ConfigService} from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(),MongooseModule.forRoot("mongodb+srv://devE:23101995@nodeexpressproject.k17nlb6.mongodb.net/dbms?retryWrites=true&w=majority&appName=NodeExpressProject"),StudentsModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }),MongooseModule.forRoot(
+    process.env.URI
+    )   
+    ,StudentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
